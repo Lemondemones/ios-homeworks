7c47250943930a2d32fc011b91b89079cb3e23b4
Minus <sergej.omelchenko.02@mail.ru>
2021-09-27 16:37:00 +0300

    Выполнено задание 1-4

---
+++ Navigation/Profile/ProfileHeaderView.swift
@@ -0,0 +1,14 @@
+//
+//  ProfileHeaderView.swift
+//  Navigation
+//
+//  Created by Sergey Omelchenko on 27.09.2021.
+//
+
+import Foundation
+import UIKit
+
+class ProfileHeaderView: UIView {
+    
+    
+}
---
+++ Navigation/Profile/ProfileViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ProfileViewController.swift
+//  Navigation
+//
+//  Created by Sergey Omelchenko on 27.09.2021.
+//
+
+import Foundation
+import UIKit
+
+class ProfileViewController: UIViewController {
+    override func viewDidLoad() {
+        view.backgroundColor = .lightGray
+    }
+}
