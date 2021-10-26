c7f6502974a67f47cb3c8ac7cd8e4f81901a0860
Minus <sergej.omelchenko.02@mail.ru>
2021-10-26 19:58:38 +0300

    Задание 2.2 выполнено

--- Navigation/Profile/ProfileViewController.swift
+++ Navigation/Profile/ProfileViewController.swift
@@ -5,22 +5,19 @@
 //  Created by Sergey Omelchenko on 27.09.2021.
 //
 
-import Foundation
 import UIKit
 
 class ProfileViewController: UIViewController {
+    
     let header = ProfileHeaderView()
     
     override func viewDidLoad() {
+        
         super.viewDidLoad()
-
         view.backgroundColor = .lightGray
         self.view.addSubview(header)
         header.addView()
-    }
-    
-    override func viewWillLayoutSubviews() {
-        guard let navigationController = navigationController else { return }
-                 header.frame = CGRect(x: view.frame.minX,y: view.frame.minY +  navigationController.navigationBar.frame.maxY,width: view.frame.width,height: view.frame.height - view.safeAreaInsets.top - view.safeAreaInsets.bottom)
+        
     }
 }
+
